60231ae09d551831d8f8252c009e329828644748
PVCcode <79192571+phamcuong8868@users.noreply.github.com>
2025-11-05 16:21:55 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<header>
+		<script>
+			//setTimeout(function(){window.location.reload();},2000);
+		</script>
+		<script src="jquery.min.js"></script>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</header>
+	
+	<body>
+		<svg x="0" y="0" width="300" heigt="600" viewbox="0 0 300 600" style="width:100%;" >
+		    <a href="http://onelink.to/k383c5"><rect x="50" y="10" width="200" height="50" rx="5" ry="5" stroke="green" fill="green" dominant-baseline="middle"/>
+		    <image href="zoom.jpg" width="40" x="3" Y="12"/>
+			<text  x="50%" y="35" width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:white;">Cài đặt phần mềm họp</text>
+			</a>
+			
+			<rect x="50" y="80" width="200" height="50" rx="5" ry="5" stroke="green" fill="green" dominant-baseline="middle"/>
+			<image href="time.svg" width="42" x="3" Y="82"/>
+			<text  x="50%" y="98" width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:yellow;">Thời gian: 6h30 - 7h45</text>
+			<text  x="50%" y="118 " width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:yellow;">Thứ 6 (ngày 15/10/2021)</text>
+			
+			<a href="https://zoom.us/j/6822022018?pwd=SDdxcGsra0M2akgvaktGelBwNW5wZz09">
+			<rect id="IdPass_Background" x="50" y="150" width="200" height="60" rx="5" ry="5" stroke="green" fill="orange" dominant-baseline="middle"/>
+			
+			<text id="Id" x="50%" y="170" width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:white;font-weight:bold;">Số (ID): 6822 022 018</text>	
+			<text id="Pass" x="50%" y="190" width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:white;">MậtKh..: 7777777 (7 số 7)</text>
+            
+            
+                <path stroke="none" fill="orange"
+                d="M 260,155 l30,25 l-30,25z" />
+                
+                <image href="joint.svg" width="40" x="3" Y="160"/>
+<image href="1.jpg" height="20" x="10" Y="520"/>
+
+<image href="2.jpg" height="20" x="50" Y="520"/>
+
+<image href="3.jpg" height="20" x="10" Y="400"/>
+
+            </a>
+            
+			<a href="phanmem/amthanh.htm">
+			<rect x="50" y="230" width="200" height="50" rx="5" ry="5" stroke="green" fill="green" dominant-baseline="middle"/>
+			<text  x="50%" y="255" width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:white;">Bật/tắt âm thanh</text></a>
+			
+			
+			<rect x="5%" y="360" width="90%" height="180" rx="5" ry="5" stroke="green" fill="none" dominant-baseline="middle"/>
+			<text  x="50%" y="380" width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:green;">LƯU Ý KHI THAM GIA HỌP</text>
+			<text  x="10%" y="415" width="100" height="50"  style="fill:green;">1. Tham gia đầy đủ, đúng giờ</text>
+			
+			<text  x="10%" y="440" width="100" height="50"  style="fill:green;">2. Đặt tên: tên Đơn vị viết tắt - Họ Và Tên</text>
+			<text  x="10%" y="465" width="100" height="50"  style="fill:green;">Ví dụ: MLTH - Nguyễn Văn A</text>
+			
+			<text  x="10%" y="490" width="100" height="50"  style="fill:green;">3. Đúng tác phong đồng phục</text>
+			
+			<text  x="10%" y="515" width="100" height="50"  style="fill:green;">4. Luôn tắt mic khi chưa phát biểu</text>
+			
+			
+			<a href="phanmem/thamgiahop.htm">
+			<rect x="50" y="300" width="200" height="50" rx="5" ry="5" stroke="green" fill="green" dominant-baseline="middle"/>
+			<text  x="50%" y="325 " width="100" height="50" dominant-baseline="middle" text-anchor="middle" style="fill:white;">Hướng dẫn vào họp</text></a>
+		</svg>
+	</body>
+	
+</html>
